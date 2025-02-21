710de7f71b215ca46e167c34cffa2f1b6bc5f876
Linus Åkesson <linus.akesson@sogeti.com>
2025-02-21 14:56:18 +0100

    managed identity test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-managed';
+export class AppComponent implements OnInit {
+  title = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('api/GetData').subscribe((data) => {
+      this.title = data.toString();
+    });
+  }
 }
